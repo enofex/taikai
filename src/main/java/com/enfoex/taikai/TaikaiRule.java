1b6d696c68c5d6e695d4979efbefb861c15d3645
mnhock <martin.hock.de@gmail.com>
2024-06-03 22:25:54 +0200

    init

--- src/main/java/com/enfoex/taikai/TaikaiRule.java
+++ src/main/java/com/enfoex/taikai/TaikaiRule.java
@@ -8,57 +8,35 @@ import java.util.Objects;
 public final class TaikaiRule {
 
   private final ArchRule archRule;
-  private final String namespace;
-  private final Namespace.IMPORT namespaceImport;
-  private final JavaClasses javaClasses;
+  private final Configuration configuration;
 
-  private TaikaiRule(ArchRule archRule, String namespace, Namespace.IMPORT namespaceImport,
-      JavaClasses javaClasses) {
+  private TaikaiRule(ArchRule archRule, Configuration configuration) {
     this.archRule = Objects.requireNonNull(archRule);
-    this.namespace = namespace;
-    this.namespaceImport = namespaceImport != null
-        ? namespaceImport : Namespace.IMPORT.WITHOUT_TESTS;
-    this.javaClasses = javaClasses;
+    this.configuration = configuration;
   }
 
   public ArchRule archRule() {
     return this.archRule;
   }
 
-  public String namespace() {
-    return this.namespace;
-  }
-
-  public Namespace.IMPORT namespaceImport() {
-    return this.namespaceImport;
-  }
-
-  public JavaClasses javaClasses() {
-    return this.javaClasses;
+  public Configuration configuration() {
+    return this.configuration;
   }
 
   public static TaikaiRule of(ArchRule archRule) {
-    return new TaikaiRule(archRule, null, Namespace.IMPORT.WITHOUT_TESTS, null);
+    return new TaikaiRule(archRule, Configuration.of());
   }
 
-  public static TaikaiRule of(ArchRule archRule, String namespace) {
-    return new TaikaiRule(archRule, namespace, Namespace.IMPORT.WITHOUT_TESTS, null);
-  }
-
-  public static TaikaiRule of(ArchRule archRule, String namespace,
-      Namespace.IMPORT namespaceImport) {
-    return new TaikaiRule(archRule, namespace, namespaceImport, null);
-  }
-
-  public static TaikaiRule of(ArchRule archRule, JavaClasses javaClasses) {
-    return new TaikaiRule(archRule, null, null, javaClasses);
+  public static TaikaiRule of(ArchRule archRule, Configuration configuration) {
+    return new TaikaiRule(archRule, configuration);
   }
 
   public void check(String globalNamespace) {
-    if (this.javaClasses != null) {
-      this.archRule.check(this.javaClasses);
+    if (this.configuration.javaClasses() != null) {
+      this.archRule.check(this.configuration.javaClasses());
     } else {
-      String namespaceToCheck = this.namespace != null ? this.namespace : globalNamespace;
+      String namespaceToCheck = this.configuration.namespace() != null
+          ? this.configuration.namespace() : globalNamespace;
 
       if (namespaceToCheck == null) {
         throw new IllegalArgumentException(
@@ -66,9 +44,52 @@ public final class TaikaiRule {
       }
 
       this.archRule.check(
-          this.namespaceImport == IMPORT.WITHOUT_TESTS
+          this.configuration.namespaceImport() == IMPORT.WITHOUT_TESTS
               ? Namespace.withoutTests(namespaceToCheck)
               : Namespace.withTests(namespaceToCheck));
+    }
+  }
+
+  public static final class Configuration {
+
+    private final String namespace;
+    private final Namespace.IMPORT namespaceImport;
+    private final JavaClasses javaClasses;
+
+    private Configuration(String namespace, Namespace.IMPORT namespaceImport,
+        JavaClasses javaClasses) {
+      this.namespace = namespace;
+      this.namespaceImport = namespaceImport != null
+          ? namespaceImport : Namespace.IMPORT.WITHOUT_TESTS;
+      this.javaClasses = javaClasses;
+    }
+
+    public String namespace() {
+      return this.namespace;
+    }
+
+    public Namespace.IMPORT namespaceImport() {
+      return this.namespaceImport;
+    }
+
+    public JavaClasses javaClasses() {
+      return this.javaClasses;
+    }
+
+    public static Configuration of() {
+      return new Configuration(null, Namespace.IMPORT.WITHOUT_TESTS, null);
+    }
+
+    public static Configuration of(String namespace) {
+      return new Configuration(namespace, Namespace.IMPORT.WITHOUT_TESTS, null);
+    }
+
+    public static Configuration of(String namespace, Namespace.IMPORT namespaceImport) {
+      return new Configuration(namespace, namespaceImport, null);
+    }
+
+    public static Configuration of(JavaClasses javaClasses) {
+      return new Configuration(null, null, javaClasses);
     }
   }
 }
