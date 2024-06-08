2f52d5b0e9eb5671a8c31881eea10483d6462e03
mnhock <martin.hock.de@gmail.com>
2024-06-08 16:46:34 +0200

    Fix deprecation rule in order to find only the usage

--- src/main/java/com/enofex/taikai/java/Deprecations.java
+++ src/main/java/com/enofex/taikai/java/Deprecations.java
@@ -1,6 +1,7 @@
 package com.enofex.taikai.java;
 
 import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaType;
 import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.SimpleConditionEvent;
@@ -14,38 +15,44 @@ final class Deprecations {
     return new ArchCondition<JavaClass>("not use deprecated APIs") {
       @Override
       public void check(JavaClass item, ConditionEvents events) {
-        if (item.isAnnotatedWith(Deprecated.class)) {
-          events.add(SimpleConditionEvent.violated(item,
-              String.format("Class %s is deprecated", item.getName())));
-        }
-
-        item.getAllFields().stream()
-            .filter(field -> field.isAnnotatedWith(Deprecated.class))
-            .forEach(field -> events.add(SimpleConditionEvent.violated(field,
-                String.format("Field %s in class %s is deprecated", field.getName(),
+        item.getFieldAccessesFromSelf().stream()
+            .filter(access -> access.getTarget().isAnnotatedWith(Deprecated.class))
+            .forEach(access -> events.add(SimpleConditionEvent.violated(access.getTarget(),
+                String.format("Field %s in class %s is deprecated and is being accessed by %s",
+                    access.getTarget().getName(), access.getTarget().getOwner().getName(),
                     item.getName()))));
 
-        item.getAllMethods().stream()
+        item.getMethodCallsFromSelf().stream()
             .filter(method -> !method.getOwner().getName().equals(Object.class.getName()))
             .filter(method -> !method.getOwner().getName().equals(Enum.class.getName()))
-            .filter(method -> method.isAnnotatedWith(Deprecated.class))
+            .filter(method -> method.getOwner().isAnnotatedWith(Deprecated.class) ||
+                method.getOwner().getRawReturnType().isAnnotatedWith(Deprecated.class) ||
+                method.getOwner().getParameterTypes().stream().anyMatch(Deprecations::isDeprecated)
+                || method.getOwner().getCallsFromSelf().stream()
+                .anyMatch(call -> call.getTarget().isAnnotatedWith(Deprecated.class)))
             .forEach(method -> events.add(SimpleConditionEvent.violated(method,
-                String.format("Method %s in class %s is deprecated", method.getName(),
+                String.format("Method %s in class %s uses deprecated APIs", method.getName(),
                     item.getName()))));
 
-        item.getAllConstructors().stream()
-            .filter(constructor -> constructor.isAnnotatedWith(Deprecated.class))
+        item.getConstructorCallsFromSelf().stream()
+            .filter(constructor -> constructor.getTarget().isAnnotatedWith(Deprecated.class) ||
+                constructor.getTarget().getParameterTypes().stream()
+                    .anyMatch(Deprecations::isDeprecated))
             .forEach(constructor -> events.add(SimpleConditionEvent.violated(constructor,
-                String.format("Constructor %s in class %s is deprecated",
-                    constructor.getFullName(), item.getName()))));
+                String.format("Constructor %s in class %s uses deprecated APIs",
+                    constructor.getTarget().getFullName(), item.getName()))));
 
         item.getDirectDependenciesFromSelf().stream()
             .filter(dependency -> dependency.getTargetClass().isAnnotatedWith(Deprecated.class))
             .forEach(dependency -> events.add(
                 SimpleConditionEvent.violated(dependency.getTargetClass(),
-                    String.format("Class %s references deprecated class %s", item.getName(),
+                    String.format("Class %s depends on deprecated class %s", item.getName(),
                         dependency.getTargetClass().getName()))));
       }
-    }.as("No usage of deprecated APIs");
+    }.as("no usage of deprecated APIs");
+  }
+
+  private static boolean isDeprecated(JavaType javaType) {
+    return javaType.toErasure().isAnnotatedWith(Deprecated.class);
   }
 }
