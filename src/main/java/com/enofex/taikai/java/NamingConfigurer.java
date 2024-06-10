67e39829a9034281b394259f1b9ae5db7aefada7
mnhock <martin.hock.de@gmail.com>
2024-06-10 20:19:00 +0200

    Provide a new Java Rule for enforcing naming conventions for constants
    Closes gb-13

--- src/main/java/com/enofex/taikai/java/NamingConfigurer.java
+++ src/main/java/com/enofex/taikai/java/NamingConfigurer.java
@@ -1,6 +1,7 @@
 package com.enofex.taikai.java;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 import com.enofex.taikai.TaikaiRule;
@@ -8,9 +9,11 @@ import com.enofex.taikai.TaikaiRule.Configuration;
 import com.enofex.taikai.configures.AbstractConfigurer;
 import com.enofex.taikai.configures.ConfigurerContext;
 import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaField;
 import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.SimpleConditionEvent;
+import java.util.regex.Pattern;
 
 public final class NamingConfigurer extends AbstractConfigurer {
 
@@ -45,6 +48,34 @@ public final class NamingConfigurer extends AbstractConfigurer {
         if (clazz.getSimpleName().startsWith("I") && Character.isUpperCase(
             clazz.getSimpleName().charAt(1))) {
           events.add(SimpleConditionEvent.violated(clazz, clazz.getSimpleName()));
+        }
+      }
+    };
+  }
+
+  public NamingConfigurer constantsShouldFollowConvention() {
+    return constantsShouldFollowConvention(null);
+  }
+
+  public NamingConfigurer constantsShouldFollowConvention(Configuration configuration) {
+    return addRule(TaikaiRule.of(fields().that().areFinal().and().areStatic()
+        .should(shouldFollowConstantNamingConvention())
+        .as("Constants should follow constant naming convention"), configuration));
+  }
+
+  private static ArchCondition<JavaField> shouldFollowConstantNamingConvention() {
+    return new ArchCondition<>("follow constant naming convention") {
+
+      private static final Pattern CONSTANT_NAME_PATTERN = Pattern.compile("^[A-Z][A-Z0-9_]*$");
+
+      @Override
+      public void check(JavaField field, ConditionEvents events) {
+        if (!field.getOwner().isEnum()
+            && !CONSTANT_NAME_PATTERN.matcher(field.getName()).matches()) {
+          String message = String.format(
+              "Constant %s in class %s does not follow the naming convention",
+              field.getName(), field.getOwner().getName());
+          events.add(SimpleConditionEvent.violated(field, message));
         }
       }
     };
