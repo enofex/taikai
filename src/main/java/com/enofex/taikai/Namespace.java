041fea5e7a685c56709eef601e31792c74aafdf8
mnhock <Martin.Hock@gfk-cps.com>
2025-03-24 16:12:07 +0100

    Speed up namespace classes import
    Closes gh-149

--- src/main/java/com/enofex/taikai/Namespace.java
+++ src/main/java/com/enofex/taikai/Namespace.java
@@ -5,8 +5,12 @@ import static java.util.Objects.requireNonNull;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.core.importer.ImportOption;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public final class Namespace {
+
+  private static final Map<Key, JavaClasses> JAVA_CLASSES = new ConcurrentHashMap<>();
 
   public enum IMPORT {
     WITHOUT_TESTS,
@@ -31,26 +35,38 @@ public final class Namespace {
   public static JavaClasses withoutTests(String namespace) {
     requireNonNull(namespace);
 
-    return new ClassFileImporter()
-        .withImportOption(new ImportOption.DoNotIncludeTests())
-        .withImportOption(new ImportOption.DoNotIncludeJars())
-        .importPackages(namespace);
+    return JAVA_CLASSES.computeIfAbsent(
+        new Key(namespace, IMPORT.WITHOUT_TESTS),
+        key -> new ClassFileImporter()
+            .withImportOption(new ImportOption.DoNotIncludeTests())
+            .withImportOption(new ImportOption.DoNotIncludeJars())
+            .importPackages(namespace)
+    );
   }
+
 
   public static JavaClasses withTests(String namespace) {
     requireNonNull(namespace);
 
-    return new ClassFileImporter()
-        .withImportOption(new ImportOption.DoNotIncludeJars())
-        .importPackages(namespace);
+    return JAVA_CLASSES.computeIfAbsent(
+        new Key(namespace, IMPORT.WITH_TESTS),
+        key -> new ClassFileImporter()
+            .withImportOption(new ImportOption.DoNotIncludeJars())
+            .importPackages(namespace));
   }
 
   public static JavaClasses onlyTests(String namespace) {
     requireNonNull(namespace);
 
-    return new ClassFileImporter()
-        .withImportOption(new ImportOption.OnlyIncludeTests())
-        .withImportOption(new ImportOption.DoNotIncludeJars())
-        .importPackages(namespace);
+    return JAVA_CLASSES.computeIfAbsent(
+        new Key(namespace, IMPORT.ONLY_TESTS),
+        key -> new ClassFileImporter()
+            .withImportOption(new ImportOption.OnlyIncludeTests())
+            .withImportOption(new ImportOption.DoNotIncludeJars())
+            .importPackages(namespace));
+  }
+
+  private record Key(String namespace, IMPORT importOption) {
+
   }
 }
