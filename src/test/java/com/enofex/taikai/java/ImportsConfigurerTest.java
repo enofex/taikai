2589da9a245d92d685a925246ceb6cd7a53b71de
mnhock <martin.hock.de@gmail.com>
2025-09-07 18:10:44 +0200

    Add missing ImportsConfigurer tests

---
+++ src/test/java/com/enofex/taikai/java/ImportsConfigurerTest.java
@@ -0,0 +1,61 @@
+package com.enofex.taikai.java;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.enofex.taikai.Taikai;
+import com.enofex.taikai.TaikaiException;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.Test;
+
+class ImportsConfigurerTest {
+
+  @Test
+  void shouldNotImportSpecificPackage() {
+    Taikai taikai = Taikai.builder()
+        .classes(new ClassFileImporter().importClasses(ClassUsingDisallowedImport.class))
+        .java(java -> java.imports(imports -> imports.shouldNotImport("java.util")))
+        .build();
+
+    assertThrows(AssertionError.class, taikai::check);
+  }
+
+  @Test
+  void shouldNotImportByRegex() {
+    Taikai taikai = Taikai.builder()
+        .classes(new ClassFileImporter().importClasses(ClassUsingDisallowedImport.class))
+        .java(java -> java.imports(
+            imports -> imports.shouldNotImport(".*ClassUsingDisallowedImport", "java\\.util\\..*")))
+        .build();
+
+    assertThrows(AssertionError.class, taikai::check);
+  }
+
+  @Test
+  void shouldThrowWhenNamespaceNotSetForCycles() {
+    assertThrows(TaikaiException.class, () -> Taikai.builder()
+        .classes(new ClassFileImporter().importClasses(ClassUsingAllowedImport.class))
+        .java(java -> java.imports(ImportsConfigurer::shouldHaveNoCycles)) // no namespace configured
+        .build());
+  }
+
+  @Test
+  void shouldAllowValidImports() {
+    Taikai taikai = Taikai.builder()
+        .classes(new ClassFileImporter().importClasses(ClassUsingAllowedImport.class))
+        .java(java -> java.imports(imports -> imports.shouldNotImport("java.sql")))
+        .build();
+
+    assertDoesNotThrow(taikai::check);
+  }
+
+  static class ClassUsingAllowedImport {
+
+    private final String value = "ok";
+  }
+
+  static class ClassUsingDisallowedImport {
+
+    private final java.util.List<String> list = java.util.Collections.emptyList();
+  }
+}
