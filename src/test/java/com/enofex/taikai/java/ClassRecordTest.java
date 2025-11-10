2a489a443f791930162e5762d891659b38180a95
Martin Hock <martin.hock.de@gmail.com>
2025-11-10 17:37:21 +0100

    Add Generic Rules to Enforce Record Usage Based on Annotation or Name Pattern
    Closes #217

---
+++ src/test/java/com/enofex/taikai/java/ClassRecordTest.java
@@ -0,0 +1,103 @@
+package com.enofex.taikai.java;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.enofex.taikai.Taikai;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class ClassRecordTest {
+
+  @Nested
+  class ClassesAnnotatedWithShouldBeRecords {
+
+    @Test
+    void shouldNotThrowWhenAnnotatedClassesAreRecords_ClassVersion() {
+      Taikai taikai = Taikai.builder()
+          .classes(new ClassFileImporter().importClasses(ValidRecord.class))
+          .java(java -> java.classesAnnotatedWithShouldBeRecords(TestAnnotation.class))
+          .build();
+
+      assertDoesNotThrow(taikai::check);
+    }
+
+    @Test
+    void shouldThrowWhenAnnotatedClassesAreNotRecords_ClassVersion() {
+      Taikai taikai = Taikai.builder()
+          .classes(new ClassFileImporter().importClasses(InvalidNonRecord.class))
+          .java(java -> java.classesAnnotatedWithShouldBeRecords(TestAnnotation.class))
+          .build();
+
+      assertThrows(AssertionError.class, taikai::check);
+    }
+
+    @Test
+    void shouldNotThrowWhenAnnotatedClassesAreRecords_StringVersion() {
+      Taikai taikai = Taikai.builder()
+          .classes(new ClassFileImporter().importClasses(ValidRecord.class))
+          .java(java -> java.classesAnnotatedWithShouldBeRecords(TestAnnotation.class.getName()))
+          .build();
+
+      assertDoesNotThrow(taikai::check);
+    }
+
+    @Test
+    void shouldThrowWhenAnnotatedClassesAreNotRecords_StringVersion() {
+      Taikai taikai = Taikai.builder()
+          .classes(new ClassFileImporter().importClasses(InvalidNonRecord.class))
+          .java(java -> java.classesAnnotatedWithShouldBeRecords(TestAnnotation.class.getName()))
+          .build();
+
+      assertThrows(AssertionError.class, taikai::check);
+    }
+  }
+
+  @Nested
+  class ClassesShouldBeRecords {
+
+    @Test
+    void shouldNotThrowWhenClassNameMatchesAndIsRecord() {
+      Taikai taikai = Taikai.builder()
+          .classes(new ClassFileImporter().importClasses(ValidRecord.class))
+          .java(java -> java.classesShouldBeRecords(".*ValidRecord"))
+          .build();
+
+      assertDoesNotThrow(taikai::check);
+    }
+
+    @Test
+    void shouldThrowWhenClassNameMatchesAndIsNotRecord() {
+      Taikai taikai = Taikai.builder()
+          .classes(new ClassFileImporter().importClasses(InvalidNonRecord.class))
+          .java(java -> java.classesShouldBeRecords(".*InvalidNonRecord"))
+          .build();
+
+      assertThrows(AssertionError.class, taikai::check);
+    }
+
+    @Test
+    void shouldNotThrowWhenNoClassNameMatches() {
+      Taikai taikai = Taikai.builder()
+          .classes(new ClassFileImporter().importClasses(ValidRecord.class))
+          .java(java -> java.classesShouldBeRecords(".*DoesNotExist"))
+          .build();
+
+      assertDoesNotThrow(taikai::check);
+    }
+  }
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @interface TestAnnotation { }
+
+  @TestAnnotation
+  record ValidRecord(String name, int id) { }
+
+  @TestAnnotation
+  static class InvalidNonRecord {
+    private final String name = "test";
+  }
+}
