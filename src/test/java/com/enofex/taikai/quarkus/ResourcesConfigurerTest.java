f4e235fc7c59ca5997214b869c861093a519e70b
Martin Hock <martin.hock.de@gmail.com>
2026-02-25 21:32:57 +0100

    Fix resource visibility test by making class non-public

--- src/test/java/com/enofex/taikai/quarkus/ResourcesConfigurerTest.java
+++ src/test/java/com/enofex/taikai/quarkus/ResourcesConfigurerTest.java
@@ -93,7 +93,7 @@ public class ResourcesConfigurerTest {
   }
 
   @Path("/test")
-  static public class PrivateResource {
+  static class PrivateResource {
   }
 
   static public class InvalidUserHandler {
